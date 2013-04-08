dabed30304d27ac8e4048b065825543e14298f65
vsync <vsync.design@gmail.com>
2013-04-08 13:19:10 +0300

    - fixed a small bug with the image 'title' / 'alt' text - converted all tabs to spaces - minor CSS Unicode convention

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 !(function(){
     'use strict';
-	// 
+
 	var numOfImages = window.location.search ? parseInt(window.location.search.match(/\d+$/)[0]) : 80;
     // Get some photos from Flickr for the demo
     $.ajax({
@@ -14,26 +14,30 @@
 	    dataType: 'jsonp',
         jsonp: 'jsoncallback'
     }).done(function (data){
-        var gallery = $('#gallery'), url;
+        var gallery = $('#gallery');
         $.each( data.photos.photo, function(index, photo){
 			// http://www.flickr.com/services/api/misc.urls.html
-            url = 'http://farm' + photo.farm + '.static.flickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret;
-			var img = $('<img>').prop({'src': url + '_t.jpg', 'title': photo.title});
+            var url = 'http://farm' + photo.farm + '.static.flickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret,
+				img = new Image();
 			
-			var link = document.createElement('a'),
-				li = document.createElement('li')
-
-			link.href = url + '_b.jpg';
-			link.appendChild(img[0]);
-			li.appendChild(link);
-			gallery[0].appendChild(li);
-
 			// lazy show the photos one by one
-			img.on('load', function(e){
+			img.onload = function(e){
+				console.log(this['largeUrl']);
+				var link = document.createElement('a'),
+				li = document.createElement('li')
+				link.href = this.largeUrl;
+				link.appendChild(this);
+				li.appendChild(link);
+				gallery[0].appendChild(li);
+			
 				setTimeout( function(){ 
 					li.className = 'loaded'; 
 				}, 20*index);
-			});
+			};
+			
+			img['largeUrl'] = url + '_b.jpg';
+			img.src = url + '_t.jpg';
+			img.title = photo.title;
         });
 
 		// finally, initialize photobox on all retrieved images
