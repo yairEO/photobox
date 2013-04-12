e8c29cfd56f9b5cdaa9e4d90cc93e01afb0b63de
vsync <vsync.design@gmail.com>
2013-04-12 13:27:20 +0300

    fixed IE8 bug in javascript

--- photobox/photobox.js
+++ photobox/photobox.js
@@ -393,7 +393,7 @@
     function getPrefixed(prop){
         var i, s = doc.createElement('p').style, v = ['ms','O','Moz','Webkit'];
         if( s[prop] == '' ) return prop;
-        prop = prop[0].toUpperCase() + prop.slice(1);
+        prop = prop.charAt(0).toUpperCase() + prop.slice(1);
         for( i = v.length; i--; )
             if( s[v[i] + prop] == '' )
                 return (v[i] + prop);
