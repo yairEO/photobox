6c3ea7e286c33bb6d820155c19f05e321ad95b64
Sean Goresht <seangores@gmail.com>
2013-12-17 22:46:51 -0700

    Update photobox to 1.8 Update index.html to actually work when debugging, with latest jquery Update demo styles to be browser-compatible for IE7+ Improve browser support using compass mixins Create additional mixin for pointer-events Add variables for caption height, image spacing, button width, and overlay colour Add cross-browser animation support Optimize some layout css using extends

--- demo/js/main.js
+++ demo/js/main.js
@@ -4,7 +4,7 @@
 //
 //-------------------------------------------------------------------------------------
 !(function(){
-    'use strict';
+	'use strict';
 
 	var numOfImages = window.location.search ? parseInt(window.location.search.match(/\d+$/)[0]) : 70,
 		gallery = $('#gallery'),
@@ -25,62 +25,63 @@
 				thumb: "http://b.vimeocdn.com/ts/431/797/431797120_100.jpg"
 			}
 		];
-		
-    // Get some photos from Flickr for the demo
-    $.ajax({
-        url: 'http://api.flickr.com/services/rest/',
-        data: {
-            format: 'json',
-            method: 'flickr.interestingness.getList',
-			per_page : numOfImages,
-            api_key: 'b51d3a7c3988ba6052e25cb152aecba2' // this is my own API key, please use yours
-        },
-	    dataType: 'jsonp',
-        jsonp: 'jsoncallback'
-    })
-	.done(function (data){
-        var loadedIndex = 1, isVideo;
-		
-		// add the videos to the collection
-		data.photos.photo = data.photos.photo.concat(videos);
-		
-        $.each( data.photos.photo, function(index, photo){
-			isVideo = photo.thumb ? true : false;
-			// http://www.flickr.com/services/api/misc.urls.html
-            var url = 'http://farm' + photo.farm + '.static.flickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret,
-				img = new Image();
-			
-			// lazy show the photos one by one
-			img.onload = function(e){
-				img.onload = null;
-				var link = document.createElement('a'),
-				li = document.createElement('li')
-				link.href = this.largeUrl;
-				link.appendChild(this);
-				if( this.isVideo ){
-					link.rel = 'video';
-					li.className = 'video'
-				}
-				li.appendChild(link);
-				gallery[0].appendChild(li);
-			
-				setTimeout( function(){ 
-					$(li).addClass('loaded');
-				}, 25*loadedIndex++);
-			};
-			
-			img['largeUrl'] = isVideo ? photo.url : url + '_b.jpg';
-			img['isVideo'] = isVideo;
-			img.src = isVideo ? photo.thumb : url + '_t.jpg';
-			img.title = photo.title;
-        });
 
-		// finally, initialize photobox on all retrieved images
-		$('#gallery').photobox('a', { thumbs:true }, callback);
-		// using setTimeout to make sure all images were in the DOM, before the history.load() function is looking them up to match the url hash
-		setTimeout(window._photobox.history.load, 1000);
-		function callback(){
-			console.log('callback for loaded content:', this);
-		};
-    });
+	// Get some photos from Flickr for the demo
+	$.ajax({
+		url: 'http://api.flickr.com/services/rest/',
+		data: {
+			format: 'json',
+			method: 'flickr.interestingness.getList',
+			per_page : numOfImages,
+			api_key: 'b51d3a7c3988ba6052e25cb152aecba2' // this is my own API key, please use yours
+		},
+		dataType: 'jsonp',
+		jsonp: 'jsoncallback'
+	})
+		.done(function (data){
+			var loadedIndex = 1, isVideo;
+
+			// add the videos to the collection
+			data.photos.photo = data.photos.photo.concat(videos);
+
+			$.each( data.photos.photo, function(index, photo){
+				isVideo = photo.thumb ? true : false;
+				// http://www.flickr.com/services/api/misc.urls.html
+				var url = 'http://farm' + photo.farm + '.static.flickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret,
+					img = document.createElement('img');
+
+				// lazy show the photos one by one
+				img.onload = function(e){
+					img.onload = null;
+					var link = document.createElement('a'),
+						li = document.createElement('li')
+					link.href = this.largeUrl;
+
+					link.appendChild(this);
+					if( this.isVideo ){
+						link.rel = 'video';
+						li.className = 'video'
+					}
+					li.appendChild(link);
+					gallery[0].appendChild(li);
+
+					setTimeout( function(){
+						$(li).addClass('loaded');
+					}, 25*loadedIndex++);
+				};
+
+				img['largeUrl'] = isVideo ? photo.url : url + '_b.jpg';
+				img['isVideo'] = isVideo;
+				img.src = isVideo ? photo.thumb : url + '_t.jpg';
+				img.title = photo.title;
+			});
+
+			// finally, initialize photobox on all retrieved images
+			$('#gallery').photobox('a', { thumbs:true }, callback);
+			// using setTimeout to make sure all images were in the DOM, before the history.load() function is looking them up to match the url hash
+			setTimeout(window._photobox.history.load, 1000);
+			function callback(){
+				console.log('callback for loaded content:', this);
+			};
+		});
 })();
