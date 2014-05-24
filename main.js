2bd8b357f3e4e880c14c517479c8699f6174dd5c
vsync <vsync.design@gmail.com>
2014-05-24 21:13:07 +0300

    css fix for video while using Modernizer in your project

--- main.js
+++ main.js
@@ -77,7 +77,7 @@
         });
 
 		// finally, initialize photobox on all retrieved images
-		$('#gallery').photobox('a', { thumbs:true }, callback);
+		$('#gallery').photobox('a', { thumbs:true, loop:false }, callback);
 		// using setTimeout to make sure all images were in the DOM, before the history.load() function is looking them up to match the url hash
 		setTimeout(window._photobox.history.load, 1000);
 		function callback(){
