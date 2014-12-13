283ec93b9ffabd57ef5b9bdb7d86a471fd59f947
vsync <vsync.design@gmail.com>
2014-12-13 18:23:16 +0200

    -

--- main.js
+++ main.js
@@ -79,7 +79,7 @@
 		// finally, initialize photobox on all retrieved images
 		$('#gallery').photobox('a', { thumbs:true, loop:false }, callback);
 		// using setTimeout to make sure all images were in the DOM, before the history.load() function is looking them up to match the url hash
-		setTimeout(window._photobox.history.load, 1000);
+		setTimeout(window._photobox.history.load, 2000);
 		function callback(){
 			console.log('callback for loaded content:', this);
 		};
