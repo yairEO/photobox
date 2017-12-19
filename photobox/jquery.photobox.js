30b019ff262817b25e40c5dc6c4ad10035aa50ff
Qathom <tony9295@gmail.com>
2017-12-19 16:21:26 +0100

    Single quotes only

--- photobox/jquery.photobox.js
+++ photobox/jquery.photobox.js
@@ -6,7 +6,7 @@
 
 
 ;(function($, doc, win){
-    "use strict";
+    'use strict';
 
     var Photobox, photobox, options, images=[], imageLinks, activeImage = -1, activeURL, lastActive, activeType, prevImage, nextImage, thumbsStripe, docElm, APControl, changeImage,
         $doc = $(doc),
@@ -15,10 +15,10 @@
         noPointerEvents = (function(){ var el = $('<p>')[0]; el.style.cssText = 'pointer-events:auto'; return !el.style.pointerEvents})(),
         isTouchDevice = false, // assume "false" unless there's a touch
         thumbsContainerWidth, thumbsTotalWidth, activeThumb = $(),
-        blankImg = "data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==",
+        blankImg = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
         transformOrigin = getPrefixed('transformOrigin'),
         transition = getPrefixed('transition'),
-        transitionend = "transitionend webkitTransitionEnd oTransitionEnd otransitionend",
+        transitionend = 'transitionend webkitTransitionEnd oTransitionEnd otransitionend',
           // Normalize rAF
         raf = window.requestAnimationFrame
            || window.webkitRequestAnimationFrame
@@ -298,7 +298,7 @@
 
         // things that should happen every time the gallery opens or closes (some messed up code below..)
         setup : function (open){
-            var fn = open ? "on" : "off";
+            var fn = open ? 'on' : 'off';
 
             // thumbs stuff
             if( options.thumbs ){
@@ -348,7 +348,7 @@
                 $win.off('resize.photobox');
             }
 
-            $doc.off("keydown.photobox")[fn]({ "keydown.photobox": keyDown });
+            $doc.off('keydown.photobox')[fn]({ 'keydown.photobox': keyDown });
 
             if( isTouchDevice ){
                 overlay.removeClass('hasArrows'); // no need for Arrows on touch-enabled
@@ -356,12 +356,12 @@
             }
 
             if( options.zoomable ){
-                overlay[fn]({"mousewheel.photobox": $.proxy(this.events.callbacks.onScrollZoom, this) });
-                if( !isOldIE) thumbs[fn]({"mousewheel.photobox": thumbsResize });
+                overlay[fn]({'mousewheel.photobox': $.proxy(this.events.callbacks.onScrollZoom, this) });
+                if( !isOldIE) thumbs[fn]({'mousewheel.photobox': thumbsResize });
             }
 
             if( !options.single && options.wheelNextPrev ){
-                overlay[fn]({"mousewheel.photobox": throttle(wheelNextPrev,1000) });
+                overlay[fn]({'mousewheel.photobox': throttle(wheelNextPrev,1000) });
             }
         },
 
@@ -804,7 +804,7 @@
             }
 
             // if there's a callback for this point:
-            if( typeof options.beforeShow == "function")
+            if( typeof options.beforeShow == 'function')
                 options.beforeShow(imageLinks[imageIndex]);
 
             overlay.removeClass('error');
@@ -863,7 +863,7 @@
         var url = images[activeImage][0],
             sign = $('<a>').prop('href',images[activeImage][0])[0].search ? '&' : '?';
         url += sign + 'vq=hd720&wmode=opaque';
-        return $("<iframe>").prop({ scrolling:'no', frameborder:0, allowTransparency:true, src:url }).attr({webkitAllowFullScreen:true, mozallowfullscreen:true, allowFullScreen:true});
+        return $('<iframe>').prop({ scrolling:'no', frameborder:0, allowTransparency:true, src:url }).attr({webkitAllowFullScreen:true, mozallowfullscreen:true, allowFullScreen:true});
     }
 
     // show the item's Title & Counter
@@ -927,7 +927,7 @@
 
         // hides the current image and prepare ground for an image change
         pbLoader.fadeOut(300, function(){
-            overlay.removeClass("pbLoading");
+            overlay.removeClass('pbLoading');
             pbLoader.removeAttr('style');
         });
         overlay.addClass('pbHide');
@@ -994,7 +994,7 @@
         var thumbList = photobox.thumbsList, h;
         thumbList.css('height', thumbList[0].clientHeight + (delta * 10) );
         h = caption[0].clientHeight / 2;
-        wrapper[0].style.cssText = "margin-top: -"+ h +"px; padding: "+ h +"px 0;";
+        wrapper[0].style.cssText = 'margin-top: -'+ h +'px; padding: '+ h +'px 0;';
         thumbs.hide().show(0);
         //thumbs.trigger('mouseenter').trigger('mousemove');
     }
@@ -1118,7 +1118,7 @@
      *
      * Requires: jQuery 1.2.2+
      */
-    !function(a){"function"==typeof define&&define.amd?define(["jquery"],a):"object"==typeof exports?module.exports=a:a(jQuery)}(function(a){function b(b){var g=b||window.event,h=i.call(arguments,1),j=0,l=0,m=0,n=0,o=0,p=0;if(b=a.event.fix(g),b.type="mousewheel","detail"in g&&(m=-1*g.detail),"wheelDelta"in g&&(m=g.wheelDelta),"wheelDeltaY"in g&&(m=g.wheelDeltaY),"wheelDeltaX"in g&&(l=-1*g.wheelDeltaX),"axis"in g&&g.axis===g.HORIZONTAL_AXIS&&(l=-1*m,m=0),j=0===m?l:m,"deltaY"in g&&(m=-1*g.deltaY,j=m),"deltaX"in g&&(l=g.deltaX,0===m&&(j=-1*l)),0!==m||0!==l){if(1===g.deltaMode){var q=a.data(this,"mousewheel-line-height");j*=q,m*=q,l*=q}else if(2===g.deltaMode){var r=a.data(this,"mousewheel-page-height");j*=r,m*=r,l*=r}if(n=Math.max(Math.abs(m),Math.abs(l)),(!f||f>n)&&(f=n,d(g,n)&&(f/=40)),d(g,n)&&(j/=40,l/=40,m/=40),j=Math[j>=1?"floor":"ceil"](j/f),l=Math[l>=1?"floor":"ceil"](l/f),m=Math[m>=1?"floor":"ceil"](m/f),k.settings.normalizeOffset&&this.getBoundingClientRect){var s=this.getBoundingClientRect();o=b.clientX-s.left,p=b.clientY-s.top}return b.deltaX=l,b.deltaY=m,b.deltaFactor=f,b.offsetX=o,b.offsetY=p,b.deltaMode=0,h.unshift(b,j,l,m),e&&clearTimeout(e),e=setTimeout(c,200),(a.event.dispatch||a.event.handle).apply(this,h)}}function c(){f=null}function d(a,b){return k.settings.adjustOldDeltas&&"mousewheel"===a.type&&b%120===0}var e,f,g=["wheel","mousewheel","DOMMouseScroll","MozMousePixelScroll"],h="onwheel"in document||document.documentMode>=9?["wheel"]:["mousewheel","DomMouseScroll","MozMousePixelScroll"],i=Array.prototype.slice;if(a.event.fixHooks)for(var j=g.length;j;)a.event.fixHooks[g[--j]]=a.event.mouseHooks;var k=a.event.special.mousewheel={version:"3.1.11",setup:function(){if(this.addEventListener)for(var c=h.length;c;)this.addEventListener(h[--c],b,!1);else this.onmousewheel=b;a.data(this,"mousewheel-line-height",k.getLineHeight(this)),a.data(this,"mousewheel-page-height",k.getPageHeight(this))},teardown:function(){if(this.removeEventListener)for(var c=h.length;c;)this.removeEventListener(h[--c],b,!1);else this.onmousewheel=null;a.removeData(this,"mousewheel-line-height"),a.removeData(this,"mousewheel-page-height")},getLineHeight:function(b){var c=a(b)["offsetParent"in a.fn?"offsetParent":"parent"]();return c.length||(c=a("body")),parseInt(c.css("fontSize"),10)},getPageHeight:function(b){return a(b).height()},settings:{adjustOldDeltas:!0,normalizeOffset:!0}};a.fn.extend({mousewheel:function(a){return a?this.bind("mousewheel",a):this.trigger("mousewheel")},unmousewheel:function(a){return this.unbind("mousewheel",a)}})});
+    !function(a){'function'==typeof define&&define.amd?define(['jquery'],a):'object'==typeof exports?module.exports=a:a(jQuery)}(function(a){function b(b){var g=b||window.event,h=i.call(arguments,1),j=0,l=0,m=0,n=0,o=0,p=0;if(b=a.event.fix(g),b.type='mousewheel','detail'in g&&(m=-1*g.detail),'wheelDelta'in g&&(m=g.wheelDelta),'wheelDeltaY'in g&&(m=g.wheelDeltaY),'wheelDeltaX'in g&&(l=-1*g.wheelDeltaX),'axis'in g&&g.axis===g.HORIZONTAL_AXIS&&(l=-1*m,m=0),j=0===m?l:m,'deltaY'in g&&(m=-1*g.deltaY,j=m),'deltaX'in g&&(l=g.deltaX,0===m&&(j=-1*l)),0!==m||0!==l){if(1===g.deltaMode){var q=a.data(this,'mousewheel-line-height');j*=q,m*=q,l*=q}else if(2===g.deltaMode){var r=a.data(this,'mousewheel-page-height');j*=r,m*=r,l*=r}if(n=Math.max(Math.abs(m),Math.abs(l)),(!f||f>n)&&(f=n,d(g,n)&&(f/=40)),d(g,n)&&(j/=40,l/=40,m/=40),j=Math[j>=1?'floor':'ceil'](j/f),l=Math[l>=1?'floor':'ceil'](l/f),m=Math[m>=1?'floor':'ceil'](m/f),k.settings.normalizeOffset&&this.getBoundingClientRect){var s=this.getBoundingClientRect();o=b.clientX-s.left,p=b.clientY-s.top}return b.deltaX=l,b.deltaY=m,b.deltaFactor=f,b.offsetX=o,b.offsetY=p,b.deltaMode=0,h.unshift(b,j,l,m),e&&clearTimeout(e),e=setTimeout(c,200),(a.event.dispatch||a.event.handle).apply(this,h)}}function c(){f=null}function d(a,b){return k.settings.adjustOldDeltas&&'mousewheel'===a.type&&b%120===0}var e,f,g=['wheel','mousewheel','DOMMouseScroll','MozMousePixelScroll'],h='onwheel'in document||document.documentMode>=9?['wheel']:['mousewheel','DomMouseScroll','MozMousePixelScroll'],i=Array.prototype.slice;if(a.event.fixHooks)for(var j=g.length;j;)a.event.fixHooks[g[--j]]=a.event.mouseHooks;var k=a.event.special.mousewheel={version:'3.1.11',setup:function(){if(this.addEventListener)for(var c=h.length;c;)this.addEventListener(h[--c],b,!1);else this.onmousewheel=b;a.data(this,'mousewheel-line-height',k.getLineHeight(this)),a.data(this,'mousewheel-page-height',k.getPageHeight(this))},teardown:function(){if(this.removeEventListener)for(var c=h.length;c;)this.removeEventListener(h[--c],b,!1);else this.onmousewheel=null;a.removeData(this,'mousewheel-line-height'),a.removeData(this,'mousewheel-page-height')},getLineHeight:function(b){var c=a(b)['offsetParent'in a.fn?'offsetParent':'parent']();return c.length||(c=a('body')),parseInt(c.css('fontSize'),10)},getPageHeight:function(b){return a(b).height()},settings:{adjustOldDeltas:!0,normalizeOffset:!0}};a.fn.extend({mousewheel:function(a){return a?this.bind('mousewheel',a):this.trigger('mousewheel')},unmousewheel:function(a){return this.unbind('mousewheel',a)}})});
 
     // Expose:
     window._photobox = {
